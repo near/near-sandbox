efd8bb3cd0ba33eb99853a731fb780ec3d242ec6
Willem Wyndham <willem@cs.umd.edu>
2021-07-23 13:47:12 -0400

    feat: spawn binary directly and not through run.js

--- npm/runner/index.js
+++ npm/runner/index.js
@@ -22,13 +22,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.runServer = void 0;
 const promisify_child_process_1 = require("promisify-child-process");
 const child_process_1 = require("child_process");
-const path_1 = require("path");
 const fs = __importStar(require("fs/promises"));
 const rimraf_1 = __importDefault(require("rimraf"));
 const util_1 = require("util");
+// @ts-ignore
+const getBinary_1 = __importDefault(require("../getBinary"));
 const rm = util_1.promisify(rimraf_1.default);
+const runFile = getBinary_1.default()._getBinaryPath();
 async function exists(d) {
     try {
         await fs.access(d);
@@ -39,50 +42,66 @@ async function exists(d) {
     return true;
 }
 async function spawn(...args) {
-    return promisify_child_process_1.spawn(path_1.join(__dirname, "run.js"), args, { encoding: 'utf8' });
+    return promisify_child_process_1.spawn(runFile, args, { encoding: 'utf8' });
 }
+const readyRegex = /Server listening at ed25519/;
 class SandboxServer {
     constructor(homeDir) {
         this.homeDir = homeDir;
     }
-    static async init(homeDir = "/tmp/sandbox") {
+    static async init(homeDir = "/tmp/near-sandbox") {
         const server = new SandboxServer(homeDir);
         if (await exists(server.homeDir)) {
             await rm(server.homeDir);
         }
-        let { stderr, code, stdout } = await server.spawn("init");
-        console.log(stdout, code);
-        console.error(stderr);
+        try {
+            let { stderr, code, stdout } = await server.spawn("init");
+            // console.log(stdout, code);
+            console.error(stderr);
+        }
+        catch (e) {
+        }
         return server;
     }
     async spawn(command) {
         return spawn('--home', this.homeDir, command);
     }
     run() {
-        try {
-            this.subprocess = child_process_1.spawn(path_1.join(__dirname, "run.js"), ['--home', this.homeDir, "run"]);
-            this.subprocess.stderr.on('data', (data) => console.log(`${data}`));
-            this.subprocess.stdout.on('data', (data) => console.log(`${data}`));
-            this.subprocess.on('exit', () => {
-                console.log("died");
-            });
-        }
-        catch (e) {
-            console.log(e);
-        }
-        return this;
+        const errors = [];
+        return new Promise((resolve, reject) => {
+            try {
+                this.subprocess = child_process_1.spawn(runFile, ['--home', this.homeDir, "run"]);
+                this.subprocess.stderr.on('data', (data) => {
+                    if (readyRegex.test(`${data}`)) {
+                        resolve(this);
+                    }
+                });
+                this.subprocess.on('exit', () => {
+                    if (errors) {
+                        console.error("Server died");
+                    }
+                });
+            }
+            catch (e) {
+                reject(e);
+            }
+        });
     }
     close() {
-        console.log(this.subprocess.kill('SIGTERM'));
+        if (!this.subprocess.kill('SIGINT')) {
+            console.error(`Failed to kill child process with PID: ${this.subprocess.pid}`);
+        }
+        process.exit(0);
     }
 }
 async function runServer() {
     const res = await SandboxServer.init();
-    res.run();
-    setTimeout(() => {
-        res.close();
-        process.exit(0);
-    }, 3000);
+    return res.run();
 }
-runServer();
-//# sourceMappingURL=runner.js.map
+exports.runServer = runServer;
+// try {
+//   runServer();
+// } catch(e: any) {
+//   console.error(e)
+// }
+//# sourceMappingURL=index.js.map
