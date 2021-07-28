6fe228be8203bd26f5e160f7f500006ad88627d9
Willem Wyndham <willem@cs.umd.edu>
2021-07-28 15:01:46 -0400

    feat: start work on runtime

--- npm/runner/src/index.ts
+++ npm/runner/src/index.ts
@@ -1,5 +1,4 @@
-
-import { join }  from "path";
+import { join } from "path";
 import tmpDir from "temp-dir";
 
 import {
@@ -12,25 +11,26 @@ import {
   copyDir,
 } from "./utils";
 
-const readyRegex = /Server listening at ed25519/
+import * as nearAPI from "near-api-js";
+
+const readyRegex = /Server listening at ed25519/;
 
 interface Config {
-  homeDir: string,
-  port: number,
-  init: boolean,
-  rm: boolean,
-  refDir: string | null
+  homeDir: string;
+  port: number;
+  init: boolean;
+  rm: boolean;
+  refDir: string | null;
 }
 
 function getHomeDir(p: number = 3000): string {
-  return join(tmpDir, 'sandbox', p.toString())
+  return join(tmpDir, "sandbox", p.toString());
 }
-
 
 // TODO: detemine safe port range
 function assertPortRange(p: number): void {
   if (p < 3000 || p > 4000) {
-    throw new Error("port is out of range, 3000-3999")
+    throw new Error("port is out of range, 3000-3999");
   }
 }
 class SandboxServer {
@@ -49,18 +49,18 @@ class SandboxServer {
       port,
       init: true,
       rm: false,
-      refDir: null
-    }
+      refDir: null,
+    };
   }
 
-  private constructor(config?: Partial<Config>){
+  private constructor(config?: Partial<Config>) {
     this._config = {
       ...SandboxServer.defaultConfig(),
       ...config,
     };
     assertPortRange(this.port);
   }
-  
+
   get config(): Config {
     return this._config;
   }
@@ -82,12 +82,12 @@ class SandboxServer {
     if (server.config.refDir) {
       await copyDir(server.config.refDir, server.config.homeDir);
     }
-    if (await exists(server.homeDir) && server.config.init) {
+    if ((await exists(server.homeDir)) && server.config.init) {
       await rm(server.homeDir);
     }
     if (server.config.init) {
       try {
-        let {stderr, code} = await server.spawn("init");
+        let { stderr, code } = await server.spawn("init");
         debug(stderr);
         if (code && code < 0) {
           throw new Error("Failed to spawn sandbox server");
@@ -102,61 +102,112 @@ class SandboxServer {
   }
 
   private async spawn(command: string): ChildProcessPromise {
-    return asyncSpawn('--home', this.homeDir, command);
+    return asyncSpawn("--home", this.homeDir, command);
   }
 
   start(): Promise<SandboxServer> {
     return new Promise((resolve, reject) => {
       try {
-        const args = ['--home', this.homeDir, "run", "--rpc-addr", this.rpcAddr];
-        debug(`sending args, ${args.join(" ")}`)
+        const args = [
+          "--home",
+          this.homeDir,
+          "run",
+          "--rpc-addr",
+          this.rpcAddr,
+        ];
+        debug(`sending args, ${args.join(" ")}`);
         this.subprocess = spawn(...args);
-        this.subprocess.stderr.on('data', (data: any) => {
+        this.subprocess.stderr.on("data", (data: any) => {
           if (readyRegex.test(`${data}`)) {
             resolve(this);
           }
         });
-        this.subprocess.on('exit',() => {
+        this.subprocess.on("exit", () => {
           debug(`Server with port ${this.port}: Died`);
         });
       } catch (e: any) {
         reject(e);
       }
-    })
+    });
   }
 
   close(): void {
-    if (!this.subprocess.kill('SIGINT')) {
-      console.error(`Failed to kill child process with PID: ${this.subprocess.pid}`)
+    if (!this.subprocess.kill("SIGINT")) {
+      console.error(
+        `Failed to kill child process with PID: ${this.subprocess.pid}`
+      );
     }
     if (this.config.rm) {
       rm(this.homeDir);
     }
   }
 }
+export class SandboxRuntime {
+  private constructor(
+    private near: nearAPI.Near,
+    private masterAccount: nearAPI.Account
+  ) {}
 
-class SandboxRuntime {
-  constructor(private rpcAddr: string){}
+  
+  // get pubKey(): any {
+  //   return this.near.keyStore.masterKey.getPublicKey();
+  // }
+
+  static async connect(
+    rpcAddr: string,
+    homeDir: string
+  ): Promise<SandboxRuntime> {
+    const near = await nearAPI.connect({
+      keyPath: join(homeDir, "validator_key.json"),
+      networkId: "sandbox",
+      nodeUrl: rpcAddr,
+    });
+    const masterAccount = new nearAPI.Account(near.connection, "test.near");
+    const runtime = new SandboxRuntime(near, masterAccount);
+    return runtime;
+  }
+
+  async createAccount(name: string): Promise<Account> {}
+
+  async createAndDeploy(name: string, wasm: string): Promise<ContractAccount> {}
+
+  async getRoot(): Promise<Account>{}
+  async getAccount(s: string): Promise<Account>{}
+
 }
 
-type TestRunnerFn = (s?: SandboxRuntime) => Promise<void> 
+type Args = { [key: string]: any };
+export class Account {
+  async call<T>(target: string, method: string, args: Args): T {}
+}
 
-async function runFunction2(configOrFunction: TestRunnerFn | Partial<Config>, fn?: TestRunnerFn): Promise<void> {
+export class ContractAcount extends Account {
+  async view<T>(method: string, args: Args): T {}
+}
+
+type TestRunnerFn = (s?: SandboxRuntime) => Promise<void>;
+
+async function runFunction2(
+  configOrFunction: TestRunnerFn | Partial<Config>,
+  fn?: TestRunnerFn
+): Promise<void> {
   // return runFunction(f)
 }
 
-async function runFunction(f:  TestRunnerFn, config?: Partial<Config>): Promise<SandboxServer> {
+async function runFunction(
+  f: TestRunnerFn,
+  config?: Partial<Config>
+): Promise<SandboxServer> {
   let server = await SandboxServer.init(config);
   await server.start(); // Wait until server is ready
   const runtime = new SandboxRuntime(server.rpcAddr);
   try {
     await f(runtime);
-  } 
-  // catch (e){
-  //   console.error(e)
-  //   throw e;
-  // } 
-  finally {
+  } finally {
+    // catch (e){
+    //   console.error(e)
+    //   throw e;
+    // }
     debug("Closing server with port " + server.port);
     server.close();
   }
@@ -165,9 +216,11 @@ async function runFunction(f:  TestRunnerFn, config?: Partial<Config>): Promise<
 
 type SandboxRunner = (f: TestRunnerFn) => Promise<void>;
 
-export async function createSandbox(setupFn: TestRunnerFn): Promise<SandboxRunner> {
+export async function createSandbox(
+  setupFn: TestRunnerFn
+): Promise<SandboxRunner> {
   const server = await runFunction(setupFn);
-  return async (testFn:  TestRunnerFn) => {
-    await runFunction(testFn, {refDir: server.homeDir, init: false});
-  }
-};
+  return async (testFn: TestRunnerFn) => {
+    await runFunction(testFn, { refDir: server.homeDir, init: false });
+  };
+}
