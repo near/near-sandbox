3e7feb251bb1f0b87de2d96538d6c678876a8b35
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-08-03 14:47:56 -0400

    fix: jest-ify tests; shorten Alice to Ali
    "Ali" copies better to slides

--- npm/tests/status-message/__tests__/main.spec.ts
+++ npm/tests/status-message/__tests__/main.spec.ts
@@ -1,45 +1,45 @@
 import { strict as assert } from "assert";
 import { createSandbox, SandboxRuntime, SandboxRunner } from "near-sandbox-runner";
 
+jest.setTimeout(15000)
+
 // sandbox creates sub-accounts of 'test.near'
-const ALICE = "alice.test.near";
+const ALI = "ali.test.near";
 const BOB = "bob.test.near";
 const CONTRACT = "status-message.test.near";
 
-// 1. Creates testing accounts and deploys a contract
-async function initSandbox() {
-  return await createSandbox(async (sandbox: SandboxRuntime) => {
+let sandboxRunner: SandboxRunner
+
+beforeAll(async () => {
+  sandboxRunner = await createSandbox(async (sandbox: SandboxRuntime) => {
     await sandbox.createAndDeploy(
       CONTRACT,
       `${__dirname}/../build/debug/status_message.wasm`
     );
-    await sandbox.createAccount(ALICE);
+    await sandbox.createAccount(ALI);
     await sandbox.createAccount(BOB);
   })
-}
+})
 
-// 2. Performs a `set_status` transaction signed by Alice and then calls `get_status` to confirm `set_status` worked
-async function testAliceSetsStatus(sandboxRunner: SandboxRunner) {
+test('Ali sets then gets status', async () => {
   await sandboxRunner(async (sandbox: SandboxRuntime) => {
-    const alice = sandbox.getAccount(ALICE);
+    const ali = sandbox.getAccount(ALI);
     const contract = sandbox.getContractAccount(CONTRACT);
-    await alice.call(CONTRACT, "set_status", { message: "hello" })
-    const result = await contract.view("get_status", { account_id: ALICE })
+    await ali.call(CONTRACT, "set_status", { message: "hello" })
+    const result = await contract.view("get_status", { account_id: ALI })
     assert.equal(result, "hello");
   })
-}
+});
 
-// 3. Gets Bob's status and which should be `null` as Bob has not yet set status
-async function testDefaultStatus(sandboxRunner: SandboxRunner) {
+test('Bob gets null status', async () => {
   await sandboxRunner(async (sandbox: SandboxRuntime) => {
     const contract = sandbox.getContractAccount(CONTRACT);
     const result = await contract.view("get_status", { account_id: BOB })
     assert.equal(result, null)
   })
-}
+});
 
-// 4. Performs a `set_status` transaction signed by Bob and then calls `get_status` to show Bob's changed status and should not affect Alice's status
-async function testStatusPerAccount(sandboxRunner: SandboxRunner) {
+test('Bob and Ali have different statuses', async () => {
   await sandboxRunner(async (sandbox: SandboxRuntime) => {
     const bob = sandbox.getAccount(BOB);
     const contract = sandbox.getContractAccount(CONTRACT);
@@ -50,21 +50,10 @@ async function testStatusPerAccount(sandboxRunner: SandboxRunner) {
     )
     assert.equal(bobStatus, "world");
 
-    const aliceStatus = await contract.view(
+    const aliStatus = await contract.view(
       "get_status",
-      { account_id: ALICE }
+      { account_id: ALI }
     )
-    assert.equal(aliceStatus, null)
+    assert.equal(aliStatus, null)
   })
-}
-
-jest.setTimeout(15000)
-
-it('works', async () => {
-  const sandboxRunner = await initSandbox()
-  await Promise.all([
-    testAliceSetsStatus(sandboxRunner),
-    testDefaultStatus(sandboxRunner),
-    testStatusPerAccount(sandboxRunner),
-  ])
-})
+});
