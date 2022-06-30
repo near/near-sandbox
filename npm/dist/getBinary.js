b2ecef51027436387530b68af42169a344916c6f
paulo20223 <paulo110895@gmail.com>
2022-07-01 00:38:50 +0300

    update binary file

--- npm/dist/getBinary.js
+++ npm/dist/getBinary.js
@@ -4,17 +4,33 @@ exports.getBinary = exports.AWSUrl = void 0;
 const _1 = require(".");
 const path_1 = require("path");
 const os = require("os");
-function getPlatform() {
-    const type = os.type();
-    const arch = os.arch();
-    if ((type === "Linux" || type === "Darwin") && arch === "x64") {
-        return [type, "x86_64"];
+function getOSTypes() {
+    return [os.type(), os.arch()];
+}
+function getVersion() {
+    const [osType, osArch] = getOSTypes();
+    if ((osType === "Linux" || osType === "Darwin") && osArch === "x64") {
+        return "master/97c0410de519ecaca369aaee26f0ca5eb9e7de06";
     }
-    throw new Error(`Unsupported platform: ${type} ${arch}`);
+    else if (osType === "Darwin" && osArch === "arm64") {
+        return "VLAD-test/c90ac67bc16ba9acbd43104eeb1ba73b6600cca9";
+    }
+    throw new Error(`Unsupported platform: ${osType} ${osArch}`);
+}
+function getPlatform() {
+    const [osType, osArch] = getOSTypes();
+    if ((osType === "Linux" || osType === "Darwin") && osArch === "x64") {
+        return [osType, "x86_64"];
+    }
+    else if (osType === "Darwin" && osArch === "arm64") {
+        return [osType, osArch];
+    }
+    throw new Error(`Unsupported platform: ${osType} ${osArch}`);
 }
 function AWSUrl() {
     const [platform, arch] = getPlatform();
-    return `https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore/${platform}-${arch}/master/97c0410de519ecaca369aaee26f0ca5eb9e7de06/near-sandbox.tar.gz`;
+    const version = getVersion();
+    return `https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore/${platform}-${arch}/${version}/near-sandbox.tar.gz`;
 }
 exports.AWSUrl = AWSUrl;
 function getBinary(name = "near-sandbox") {
--- npm/src/getBinary.ts
+++ npm/src/getBinary.ts
@@ -2,19 +2,34 @@ import { Binary } from ".";
 import { join } from "path";
 import * as os from "os";
 
-function getPlatform() {
-  const type = os.type();
-  const arch = os.arch();
+function getOSTypes() {
+  return [os.type(), os.arch()]
+}
 
-  if ((type === "Linux" || type === "Darwin") && arch === "x64") {
-    return [type, "x86_64"];
+function getVersion() {
+  const [osType, osArch] = getOSTypes()
+  if ((osType === "Linux" || osType === "Darwin") && osArch === "x64") {
+      return "master/97c0410de519ecaca369aaee26f0ca5eb9e7de06"
+  } else if (osType === "Darwin" && osArch === "arm64") {
+      return "VLAD-test/c90ac67bc16ba9acbd43104eeb1ba73b6600cca9";
   }
-  throw new Error(`Unsupported platform: ${type} ${arch}`);
+  throw new Error(`Unsupported platform: ${osType} ${osArch}`);
+}
+
+function getPlatform() {
+  const [osType, osArch] = getOSTypes()
+  if ((osType === "Linux" || osType === "Darwin") && osArch === "x64") {
+      return [osType, "x86_64"];
+  } else if (osType === "Darwin" && osArch === "arm64") {
+      return [osType, osArch];
+  }
+  throw new Error(`Unsupported platform: ${osType} ${osArch}`);
 }
 
 export function AWSUrl(): string {
   const [platform, arch] = getPlatform();
-  return `https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore/${platform}-${arch}/master/97c0410de519ecaca369aaee26f0ca5eb9e7de06/near-sandbox.tar.gz`;
+  const version = getVersion();
+  return `https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore/${platform}-${arch}/${version}/near-sandbox.tar.gz`;
 }
 
 export function getBinary(name: string = "near-sandbox"): Promise<Binary> {
