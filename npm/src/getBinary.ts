d21cb210d7dad60980867d09f77ddd80e8c2773a
Phuong Nguyen <phuong@near.org>
2022-08-12 11:25:27 -0700

    Bump nearcore bin to fix macos overflow

--- npm/src/getBinary.ts
+++ npm/src/getBinary.ts
@@ -17,7 +17,7 @@ function getPlatform() {
 
 export function AWSUrl(): string {
   const [platform, arch] = getPlatform();
-  return `https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore/${platform}-${arch}/master/13a66dda709a4148f6395636914dca2a55df1390/near-sandbox.tar.gz`;
+  return `https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore/${platform}-${arch}/master/dbb72eba04dcdda3fc97953af8be9870314e53dc/near-sandbox.tar.gz`;
 }
 
 export function getBinary(name: string = "near-sandbox"): Promise<Binary> {
